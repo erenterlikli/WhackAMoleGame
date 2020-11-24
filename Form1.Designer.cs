d9b1d599000e0ae2d4dc4f25705ee0b85d05b9c9
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-11-24 16:30:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace KöstebekOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(13, 13);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(805, 371);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(348, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puan:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(424, 387);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(840, 433);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Köstebek Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KöstebekOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int puan = 0; 
+        Random rnd = new Random();
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            for(int i=1;i<85;i++)
+            {
+                Button btn = new Button();
+                btn.ForeColor = Color.Black;
+                btn.BackColor = Color.White;
+                btn.Width = 50;
+                btn.Height = 50;
+                btn.Text = i.ToString();
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            int rastgele = rnd.Next(1, 85);
+            foreach(var a in flowLayoutPanel1.Controls)
+            {
+                Button btn = a as Button;
+                if(btn.Text==rastgele.ToString()) //rastgele bir buton yansın.
+                {
+                    btn.BackColor = Color.Red;
+                    
+                        btn.Click += new EventHandler(Btn_Click);
+                    
+                }
+
+                else
+                {
+                    btn.BackColor = Color.White;
+                }
+                  
+            }
+        }
+
+         void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+          if(btn.BackColor==Color.Red)
+            {
+                puan++;
+                label2.Text = puan.ToString();
+            }
+            else
+            {
+                puan--;
+                label2.Text = puan.ToString();
+            }
+        }
+    }
+}
